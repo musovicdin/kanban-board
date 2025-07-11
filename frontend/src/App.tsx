27dbcf817bc7c23c5f6df987c2162c9f81a7c2b2
musovicdin <dinmusovic1@gmail.com>
2025-07-11 12:27:37 +0200

    feat: frontend setup completed (tailwind, ts, prettier etc.)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-24 w-24 bg-blue-500">
+        <p className="text-red-400">Setup Test!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
